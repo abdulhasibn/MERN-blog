af6ef7fa968353bb15ce88afdc2e923ef818875d
Abdul Hasib <abdulhasibn@gmail.com>
2024-01-20 09:40:48 +0530

    Create function to connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+config();
 
-app.listen(3000, () => {
-  console.log("Server is running on Port 3000!");
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello" });
 });
+
+const PORT = process.env.PORT || 3000;
+
+const MONGO_STRING = process.env.MONGO_STRING;
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(MONGO_STRING);
+    app.listen(PORT, () => {
+      console.log(`Connected to DB and server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectToDb();
