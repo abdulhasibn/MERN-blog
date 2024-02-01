e4924c0a2011ddcb5059f898c97f157d6be30138
Abdul Hasib <abdulhasibn@xbi4.com>
2024-02-01 19:23:41 +0530

    Complete sidebar for dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowRight } from "react-icons/hi";
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div>
+      <Sidebar className="w-full md:w-56">
+        <Sidebar.Items>
+          <Sidebar.ItemGroup className="flex flex-col gap-2">
+            <Link to={"/dashboard?tab=profile"}>
+              <Sidebar.Item
+                active={tab === "profile"}
+                icon={HiUser}
+                label="User"
+                labelColor="dark"
+              >
+                Profile
+              </Sidebar.Item>
+            </Link>
+            <Link to={"/sign-in"}>
+              <Sidebar.Item icon={HiArrowRight} labelColor="dark">
+                Sign Out
+              </Sidebar.Item>
+            </Link>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </div>
+  );
+}
